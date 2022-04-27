4a2b1d80ed75e70711716dfd826d558ab260d651
mariaclarabez <mariaclarabez6@gmail.com>
2022-04-27 14:57:15 -0400

    everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import './App.css';
-import './index.css';
 import './vendors/bootstrap/css/bootstrap.min.css';
-import './vendors/fontawesome/css/all.min.css';
 // import './vendors/bootstrap/bootstrap.min.css';
 import CharacterView from './components/character/characterView';
 import HomeView from './components/login/homeView';
-import HomeScreen from './components/home';
-import ProfileScreen from './components/profile';
-import WikiScreen from './components/wiki';
 import { Routes, Route } from "react-router-dom"
-import './index.css';
 
 function App() {
     return (
       <div className="App">
         <Routes>
           <Route path="/" element={ <HomeView/> } />
-          <Route path="/home" element={<HomeScreen/>}/>
           <Route path="create" element={ <CharacterView/> } />
-          <Route path="/profile" element={<ProfileScreen/>}/>
-          <Route path="/wiki" element={<WikiScreen/>}/>
+          <Route path="create/user/:userId" element={<CharacterView />} />
         </Routes>
       </div>
     )
